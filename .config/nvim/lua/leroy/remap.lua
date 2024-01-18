ee049b05b0b47437a209116301e2eeeb3ffe8f27
lpdkt <leroy.traebert@gmx.de>
2024-01-18 22:13:23 +0100

    update

--- .config/nvim/lua/leroy/remap.lua
+++ .config/nvim/lua/leroy/remap.lua
@@ -16,7 +16,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
-
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
@@ -31,7 +30,3 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>cf", "<cmd>e ~/.config/nvim/lua/leroy/plugins/<CR>");
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/master
--- .config/nvim/lua/leroy/set.lua
+++ .config/nvim/lua/leroy/set.lua
@@ -30,15 +30,13 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-<<<<<<< HEAD
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
-=======
+
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
->>>>>>> refs/remotes/origin/master
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
