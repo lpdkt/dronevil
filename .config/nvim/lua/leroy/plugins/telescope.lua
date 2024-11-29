7404298faf9a88c7ed31c34ab1248f6a0bc5d729
lpdkt <data@lpdkt.xyz>
2024-11-29 21:01:54 +0100

    system in tokyo night theme on different branch

--- .config/nvim/lua/leroy/plugins/telescope.lua
+++ .config/nvim/lua/leroy/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     dependencies = { { 'nvim-lua/plenary.nvim' } },
     config = function()
         local builtin = require('telescope.builtin')
+        vim.g.tokyonight_dark_float = false
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fs', function()
