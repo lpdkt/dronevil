fc9d5ee6c4ae2ef1d2e4a252e05892e824e9cf32
lpdkt <data@lpdkt.xyz>
2024-11-29 10:15:48 +0100

    update

--- .config/nvim/lua/leroy/plugins/init.lua
+++ .config/nvim/lua/leroy/plugins/init.lua
@@ -1,8 +1,6 @@
 return {
     'mbbill/undotree',
     'tpope/vim-fugitive',
-    -- 'xiyaowong/transparent.nvim',
-    'eandrju/cellular-automaton.nvim',
     -- 'github/copilot.vim',
 
     { 'wakatime/vim-wakatime', lazy = false },
