7100fc1f1163e9ac52bd64a05c32791cfdac3d54
lpdkt <data@lpdkt.xyz>
2024-11-29 18:11:09 +0100

    update

--- .config/nvim/lua/leroy/plugins/color.lua
+++ .config/nvim/lua/leroy/plugins/color.lua
@@ -1,11 +1,11 @@
 return {
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000,
-    config = function ()
-	    require("gruvbox").setup({
-		    overrides = {},
-		    dim_inactive = false,
-		    transparent_mode = true,
-	    })
-    end
+	'ellisonleao/gruvbox.nvim',
+	priority = 1000,
+	config = function()
+		require("gruvbox").setup({
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = true,
+		})
+	end,
 }
