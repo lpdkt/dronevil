ef9fef2224420e21fb6c5dc50f2bfb5266f4188a
lpdkt <lpdkt@pm.me>
2025-09-23 20:35:36 +0200

    update

--- modules/home/desktop/hyprland/hyprpaper.nix
+++ modules/home/desktop/hyprland/hyprpaper.nix
@@ -3,8 +3,8 @@
     enable = true;
     settings = {
       splash = false;
-      preload = [ "~/.config/dronevil/modules/home/desktop/hyprland/wallpaper/mima-1440.png" ];
-      wallpaper = [ ",~/.config/dronevil/modules/home/desktop/hyprland/wallpaper/mima-1440.png" ];
+      preload = [ "~/.config/dronevil/modules/home/desktop/hyprland/wallpaper/nixflake.png" ];
+      wallpaper = [ ",~/.config/dronevil/modules/home/desktop/hyprland/wallpaper/nixflake.png" ];
     };
   };
 }
